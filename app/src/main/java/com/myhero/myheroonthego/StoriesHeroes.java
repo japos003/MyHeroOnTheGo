56d39b188de78312dfa10ade1acdf4771ff958a0
Jon Apostol <japostol@uci.edu>
2015-02-24 23:34:35 -0800

    Edited to Check if Github Works

--- app/src/main/java/com/myhero/myheroonthego/StoriesHeroes.java
+++ app/src/main/java/com/myhero/myheroonthego/StoriesHeroes.java
@@ -11,7 +11,7 @@ import android.view.View;
 //import android.widget.ListView;
 import android.widget.TextView;
 //import android.widget.Toast;
-
+//test
 
 public class StoriesHeroes extends ActionBarActivity {
 
