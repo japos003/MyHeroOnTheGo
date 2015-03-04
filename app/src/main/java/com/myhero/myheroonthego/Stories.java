898757f3c250e7aea1348f15e9f36b847bca48d3
Jon Apostol <japostol@uci.edu>
2015-03-03 21:40:02 -0800

    -Added class SubmitStory -Stories Class's Menu now has SubmitStory button

--- app/src/main/java/com/myhero/myheroonthego/Stories.java
+++ app/src/main/java/com/myhero/myheroonthego/Stories.java
@@ -61,7 +61,7 @@ public class Stories extends ActionBarActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_stories, menu);
         return true;
     }
 
@@ -73,8 +73,8 @@ public class Stories extends ActionBarActivity{
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.submitStory) {
+            startActivity(new Intent(Stories.this, SubmitStory.class));
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/myhero/myheroonthego/SubmitStory.java
+++ app/src/main/java/com/myhero/myheroonthego/SubmitStory.java
@@ -1,29 +1,28 @@
 package com.myhero.myheroonthego;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.content.Intent;
 
 
-public class StartUp extends ActionBarActivity {
+public class SubmitStory extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_up);
+        setContentView(R.layout.activity_submit_story);
     }
 
-    /*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start_up, menu);
+        getMenuInflater().inflate(R.menu.menu_submit_story, menu);
         return true;
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -39,20 +38,14 @@ public class StartUp extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    */
 
     //VIDEOS
-    public void gotoFilms(View view){
-
+    public void gotoFilms(View view) {
+        //startActivity(new Intent(DisplayStories.this, Films.class));
     }
 
     //ART
-    public void gotoArt(View view){
-        startActivity(new Intent(StartUp.this, Art.class));
-    }
-
-    //STORIES
-    public void gotoStories(View view){
-        startActivity(new Intent(StartUp.this, Stories.class));
+    public void gotoArt(View view) {
+        startActivity(new Intent(SubmitStory.this, Art.class));
     }
 }
