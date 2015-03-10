f0f096d9ac98c0c161cbd646b3738983a8ba0a22
Ryan Morrison <morrisor@uci.edu>
2015-03-09 23:16:36 -0700

    updated AllFilms

--- app/src/main/java/com/myhero/myheroonthego/AllFilms.java
+++ app/src/main/java/com/myhero/myheroonthego/AllFilms.java
@@ -1,11 +1,9 @@
 package com.myhero.myheroonthego;
 
-import java.io.Serializable;
-
 /**
  * Created by Jon Apostol on 3/6/2015.
  */
-public class AllFilms implements Serializable {
+public class AllFilms {
     public String getFilmLink() {
         return movielink;
     }
@@ -22,9 +20,23 @@ public class AllFilms implements Serializable {
 
     private String name;
     private String movielink;
+    private String videolink;
+    private String videoDesc;
+
+    public String getVideoLink() {return videolink;}
+    public void setVideoLink(String videolink) {this.videolink = videolink;}
+
+    public String getVideoDesc() {return videoDesc;}
+    public void setVideoDesc(String videoDesc) {this.videoDesc = videoDesc;}
 
     @Override
-   public String toString() {
-        return "name:" + name  + "~ movielink:" + movielink;
+    public String toString() {
+        return "Title:" + name;  //+ "~ movielink:" + movielink;
     }
+
+    public String getLink() { return videolink; }
+    public String getDesc() { return videoDesc; }
+    public String getTitle() { return name; }
+
+
 }
