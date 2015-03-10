332dc58580e1e9b11f5dec000fcf0ff0dd79d5b6
Ryan Morrison <morrisor@uci.edu>
2015-03-09 23:14:21 -0700

    updated Films

--- app/src/main/java/com/myhero/myheroonthego/Films.java
+++ app/src/main/java/com/myhero/myheroonthego/Films.java
@@ -1,27 +1,42 @@
 package com.myhero.myheroonthego;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class Films extends ActionBarActivity {
 
     private ArrayList<AllFilms> films;
-    private ArrayList<WantedFilm> fm;
-    private String filmtag = " ";
-    private String filmlink = "http://198.199.112.105/mailbox_lg.mp4";
+    //private String video_url = "http://198.199.112.105/";
+    private String video_url;
+    private String video_desc;
+    private  String video_title;
+    //private String filmtag = " ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +51,20 @@ public class Films extends ActionBarActivity {
         saf.execute();
     }
 
-    class SearchAllFilms extends AsyncTask<String, Integer, ArrayList<AllFilms2>> {
+    class SearchAllFilms extends AsyncTask<String, Integer, ArrayList<AllFilms>> {
         //call to get a list of all story tags
         @Override
-        protected ArrayList<AllFilms2> doInBackground(String... params) {
-            executeGetAllFilms2 getallfilms2 = new executeGetAllFilms2();
+        protected ArrayList<AllFilms> doInBackground(String... params) {
             executeGetAllFilms getallfilms = new executeGetAllFilms();
             //put the list of stories into an array
-            ArrayList<AllFilms2> films2 = getallfilms2.GetAllMovies2();
             films = getallfilms.GetAllMovies();
-            return films2;
+            return films;
         }
 
         @Override
-        protected void onPostExecute(ArrayList<AllFilms2> allFilms2) {
+        protected void onPostExecute(ArrayList<AllFilms> allFilms2) {
             //once we get all stories, call to display as a list
-            ArrayAdapter<AllFilms2> filmAdapter = new ArrayAdapter<AllFilms2>(Films.this, android.R.layout.simple_list_item_1, allFilms2);
+            ArrayAdapter<AllFilms> filmAdapter = new ArrayAdapter<AllFilms>(Films.this, android.R.layout.simple_list_item_1, films);
             setProgressBarIndeterminateVisibility(false);
             setContentView(R.layout.activity_films);
             //this function is to see if user clicks on anything in the list
@@ -67,45 +80,7 @@ public class Films extends ActionBarActivity {
 
     }
 
-
-    class SearchFilm extends AsyncTask<String, Integer, ArrayList<WantedFilm2>> {
-        //call to get a list of all story tags
-        @Override
-        protected ArrayList<WantedFilm2> doInBackground(String... params) {
-            executeGetFilm2 getfilm2 = new executeGetFilm2();
-            executeGetFilm getfilm = new executeGetFilm();
-            //put the list of stories into an array
-            ArrayList<WantedFilm2> film2 = getfilm2.GetFilm2(filmtag);
-            fm = getfilm.GetFilm(filmtag);
-            return film2;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<WantedFilm2> wantedfilm2) {
-            //once we get all stories, call to display as a list
-            ArrayAdapter<WantedFilm2> filmAdapter = new ArrayAdapter<WantedFilm2>(Films.this, android.R.layout.simple_list_item_1, wantedfilm2);
-            setProgressBarIndeterminateVisibility(false);
-            setContentView(R.layout.activity_films);
-            //this function is to see if user clicks on anything in the list
-            registerClickCallback();
-            //This function is to make the list of story tags visible
-            populateListView2(filmAdapter);
-        }
-        //doesn't really do anything
-        @Override
-        protected void onPreExecute() {
-            setProgressBarIndeterminateVisibility(true);
-        }
-
-    }
-
-    private void populateListView(ArrayAdapter<AllFilms2> a) {
-        //this is to set the view to display the list of stroy tags
-        ListView list = (ListView) findViewById(R.id.listView);
-        list.setAdapter(a);
-    }
-
-    private void populateListView2(ArrayAdapter<WantedFilm2> a) {
+    private void populateListView(ArrayAdapter<AllFilms> a) {
         //this is to set the view to display the list of stroy tags
         ListView list = (ListView) findViewById(R.id.listView);
         list.setAdapter(a);
@@ -120,14 +95,30 @@ public class Films extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> paret, View viewClicked, int position, long id) {
                 TextView textView = (TextView) viewClicked;
-                //String message ="Which is string: " + textView.getText().toString();
+                String message ="Which is string: " + textView.getText().toString();
                 //String tosplit = textView.getText().toString();
                 String tosplit = " ";
-                if(films != null && !films.isEmpty()) {
+                String link = "";
+                String desc = "";
+                String title = "";
+
+                if(films != null) {
                     tosplit = films.get(position).toString();
+                    link = films.get(position).getLink();
+                    video_url = link;
+                    desc = films.get(position).getDesc();
+                    video_desc = desc;
+                    title = films.get(position).getTitle();
+                    video_title = title;
+
+                    Intent i = new Intent(Films.this, VideoActivity.class);
+                    i.putExtra("video_url",link);
+                    i.putExtra("desc", desc);
+                    i.putExtra("title", title);
+                    startActivity(i);
                 }
-                if(fm != null && !fm.isEmpty()) {
-                    tosplit = fm.get(position).toString();
+                else {
+
                 }
                 //if (textView.getText().toString().contains("~")) {
                 if (tosplit.contains("~")) {
@@ -136,22 +127,15 @@ public class Films extends ActionBarActivity {
                     if (tag.contains(":")) {
                         String[] xparts = tag.split(":");
                         String tag2 = xparts[1];
-                        filmtag = films.get(position).getFilmLink().toString();
-                        films.clear();
-                        SearchFilm sf = new SearchFilm();
-                        sf.execute();
-                        //Ryan the link to the movie the user wants to play is saved in filmlink
-                        //for now it is a hardcoded string I will work on it later to get the acutal link by tonight
-                        //execute to the play the video here
-                        //-Raul
-                        //Toast.makeText(Films.this, tag2.trim(), Toast.LENGTH_LONG).show();
+                       // Toast.makeText(Films.this, tag2.trim(), Toast.LENGTH_LONG).show();
+                        //Toast.makeText(Films.this, link, Toast.LENGTH_LONG).show();
                     }
                     else {
 
                     }
                 }
                 else {
-                    //Toast.makeText(Films.this, message, Toast.LENGTH_LONG).show();
+                    Toast.makeText(Films.this, message, Toast.LENGTH_LONG).show();
                 }
             }
         });
